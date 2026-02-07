972a81a30f8a411c837b8b2f63b8babc761dcf09
Ednah Bridget <bidgetkakah@gmail.com>
2026-02-08 00:36:04 +0300

    Implement categories CRUD API with JWT auth

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission: Admins can edit, others can only read.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+    lookup_field = 'id'  # Use UUID
