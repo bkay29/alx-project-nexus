dd103c684911cade11f65b67e2e0e37e7c931e1b
Ednah Bridget <bidgetkakah@gmail.com>
2026-02-08 01:25:24 +0300

    Implement products CRUD API with filtering and admin permissions.

--- ecommerceconfig/urls.py
+++ ecommerceconfig/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("users.urls")),
     path('api/categories/', include('categories.urls')),
+    path('api/products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Product
+from .serializers import ProductSerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    lookup_field = 'id'
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at']
